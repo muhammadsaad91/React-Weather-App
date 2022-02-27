3a4e14bdd664ec73f47ddeb5bc291d2c38c3b32a
muhammadsaad91 <saadattari91@gmail.com>
2022-02-27 20:30:58 +0500

    complete weather App responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import react, { useState, useEffect } from "react";
 
 const App = () => {
-    const[name , setName]=useState('');
+    const[name , setName]=useState("Karachi");
     const [temp , setTemp]=useState('');
     const [submit , setSubmit]=useState('');
      
@@ -13,15 +13,18 @@ const App = () => {
 const data =  async () => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=202d47ca85b13f6dce8d382b6fbc8e6f`);
     const data = await response.json();
-    {temp=== null? document.getElementById('pa').innerHTML='not found' :setTemp(data.main.temp)}
+    {data.cod==400? alert('Please write Something') :setTemp(data.main.temp)}
+    console.log(data);
+    // setTemp(data.main.temp)
    setSubmit(name);
 }
 
-const clicked = (e) => {
+const Clicked = (e) => {
      data();
-
-    setName('');
+     setName('');
 }
+// not found hook 
+
 
 
 
@@ -32,12 +35,11 @@ return (
            <div className="header"> <h1> SaAd Weather App</h1></div>
        <div className="flex-inp">  
         <div className="input-div"><input type="text" placeholder="Enter City Name"  value={name} onChange={change}/></div>
-        <button onClick={clicked}>+</button>
+        <button onClick={Clicked}>🔍</button>
         </div> 
         <div className="name"> <h1>{submit} </h1>
         {/* {name.length===''?<h2>Please Write Any City Name To get the current weather</h2>:null }  */}
-       {temp===''? null:<p id="pa">{temp}°C</p>}
-
+        {temp===''?null : <h2>{temp}°C</h2>}
         </div>
 
 
